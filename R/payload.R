681c5623e076e9a0472795453dab405c2726002f
Sasha Pejicic <52110058+The-R-Man@users.noreply.github.com>
2020-08-26 10:48:50 +0200

    Update payload.R

--- R/payload.R
+++ R/payload.R
@@ -1,8 +1,39 @@
+
+#' @title NORDCAN Statistics Payload
+#' @description
+#' Compute all necessary statistics for NORDCAN into a single payload.
+#' @param datasets `[list]` (mandatory, no default)
+#'
+#' a list containing datasets. the following are mandatory:
+#' - `cancer_record_dataset`: each row is a cancer record as output by
+#'   [nordcanpreprocessing::nordcan_incidence_dataset]
+#' - `population_mean_size_dataset`: see
+#'   [nordcanpreprocessing::nordcan_population_mean_size_dataset]
+#' - `population_mortality_dataset`: see
+#'   [nordcanpreprocessing::nordcan_population_mortality_dataset]
+#'
+#' the following are optional and are computed within this function if not
+#' supplied:
+#' - `cancer_death_count_dataset`: contains the numbers of cancer deaths; see
+#'   [nordcanpreprocessing::nordcan_cancer_death_count_dataset]
+#' @examples
+#'
+#' nc_stats_payload <- nordcanepistats::nordcan_statistics_payload(
+#'   datasets = list(
+#'     cancer_record_dataset = my_crd,
+#'     cancer_death_count_dataset = my_cdcd,
+#'     population_mean_size_dataset = my_mps,
+#'     population_mortality_dataset = my_md
+#'   )
+#' )
+#' @export
+
+####functions
 nordcan_statistics_payload <- function(
   datasets=list(
-    mortality_count=mortality_count,
-    mortality_raw=mortality_raw,
-    incidence=incidence
+    cancer_death_count_dataset=cancer_death_count_dataset,
+    cancer_record_dataset=cancer_record_dataset,
+    cancer_case_dataset=cancer_case_dataset
   )
 ) {
   
@@ -10,32 +41,38 @@ nordcan_statistics_payload <- function(
     inherits(datasets, "list"),
     vapply(datasets, data.table::is.data.table, logical(1L)),
     !is.null(names(datasets)),
-    names(datasets) %in% c("mortality_count","mortality_raw","incidence"),
-    c("mortality_raw","incidence") %in% names(datasets)
+    names(datasets) %in% c("cancer_death_count_dataset","cancer_record_dataset","cancer_case_dataset"),
+    c("cancer_record_dataset","cancer_case_dataset") %in% names(datasets)
   )
   
   '%ni%'=Negate('%in%')
-  if('mortality_count' %in% names(datasets)){message("mortality_count is given in datasets and need not to be computed inside this function!")} else if('mortality_count' %ni% names(datasets)) {mortality_count=unique(stat_count(x=mortality_raw,by=mortality_raw))}
+  if('cancer_death_count_dataset' %in% names(datasets)){
+  message("cancer_death_count_dataset is given in datasets and 
+  need not to be computed inside this function!")
+  } 
+  else if('cancer_death_count_dataset' %ni% names(datasets)){
+  cancer_death_count_dataset=unique(
+  basicepistats::stat_count(x=cancer_record_dataset,by=cancer_record_dataset)
+  )
+  }
   
   payload=list(
-    mortality_count=mortality_count,
-  incidence_stat_table=as.data.table(stat_table_list(
+    cancer_death_count_dataset=cancer_death_count_dataset,
+    cancer_case_count_dataset=data.table::as.data.table(basicepistats::stat_table_list(
     varying_arg_list = list(
     by = list(c("year","sex","region","age","entity"))
   ),
-  fixed_arg_list = list(x = incidence),
+  fixed_arg_list = list(x = cancer_case_dataset),
   stat_fun_nm = "stat_count"
 ))
   )
   
   stopifnot(
-     c("mortality_count","incidence_stat_table")
+     c("cancer_death_count_dataset","cancer_case_count_dataset")
       %in%
       names(payload),
     inherits(payload, "list"),
     vapply(payload, inherits, logical(1L), what = "data.table")
   )
-  write.csv(mortality_count,"Result Mortality.csv",row.names=F)
-  write.csv(mortality_count,"Result Incidence.csv",row.names=F)
   return(payload)
 }
