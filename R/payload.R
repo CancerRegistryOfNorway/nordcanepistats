bde2ab5d89b9b71bf7f8fbaecd68a8066c1bb8e5
Sasha Pejicic <52110058+the-r-man@users.noreply.github.com>
2020-09-02 11:49:03 +0200

    Update payload.R

--- R/payload.R
+++ R/payload.R
@@ -31,6 +31,9 @@
 # ####working directory
 # setwd("")
 #  
+# ####packages
+# library(basicepistats)
+#
 # ####data
 #
 # ###mortality
@@ -39,7 +42,8 @@
 # 
 # ###incidence
 # cancer_case_dataset=data.table::data.table(read.csv("Cancer_case_dataset.csv"))
-
+# cancer_case_dataset$date_of_incidence=as.Date(cancer_case_dataset$date_of_incidence,"%d.%m.%Y")
+# cancer_case_dataset$end_of_followup=as.Date(cancer_case_dataset$end_of_followup,"%d.%m.%Y")
 
 nordcan_statistics_payload <- function(
   datasets=list(
@@ -76,7 +80,57 @@ nordcan_statistics_payload <- function(
   ),
   fixed_arg_list = list(x = cancer_case_dataset),
   stat_fun_nm = "stat_count"
-))
+)),
+    nordcan_statistics_payload <- function(
+  datasets=list(
+    cancer_death_count_dataset=cancer_death_count_dataset,
+    cancer_record_dataset=cancer_record_dataset,
+    cancer_case_dataset=cancer_case_dataset
+  )
+) {
+  
+  stopifnot(
+    inherits(datasets, "list"),
+    vapply(datasets, data.table::is.data.table, logical(1L)),
+    !is.null(names(datasets)),
+    names(datasets) %in% c("cancer_death_count_dataset","cancer_record_dataset","cancer_case_dataset"),
+    c("cancer_record_dataset","cancer_case_dataset") %in% names(datasets)
+  )
+  
+  '%ni%'=Negate('%in%')
+  if('cancer_death_count_dataset' %in% names(datasets)){
+    message("cancer_death_count_dataset is given in datasets and 
+            need not to be computed inside this function!")
+  } 
+  else if('cancer_death_count_dataset' %ni% names(datasets)){
+    cancer_death_count_dataset=unique(
+      basicepistats::stat_count(x=cancer_record_dataset,by=cancer_record_dataset)
+    )
+  }
+  
+  payload=list(
+    cancer_death_count_dataset=cancer_death_count_dataset,
+    cancer_case_count_dataset=data.table::as.data.table(basicepistats::stat_table_list(
+      varying_arg_list = list(
+        by = list(c("year","sex","region","age","entity"))
+      ),
+      fixed_arg_list = list(x = cancer_case_dataset),
+      stat_fun_nm = "stat_count"
+    )),
+  cancer_prevalence=data.table::as.data.table(
+  nordcanepistats::nordcanstat_prevalent_subject_count(cancer_case_dataset)
+  )
+  )
+  
+  stopifnot(
+    c("cancer_death_count_dataset","cancer_case_count_dataset")
+    %in%
+      names(payload),
+    inherits(payload, "list"),
+    vapply(payload, inherits, logical(1L), what = "data.table")
+  )
+  return(payload)
+}
   )
   
   stopifnot(
