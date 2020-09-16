922608cea3361230593303c5d3e4511421d40f6b
Joonas Miettinen <joonas.m.miettinen@gmail.com>
2020-09-16 12:04:02 +0300

    improved nordcan_statistics_tables docs

--- R/payload.R
+++ R/payload.R
@@ -4,27 +4,63 @@
 #' Compute all necessary statistics for NORDCAN into a single payload.
 #' @param datasets `[list]` (mandatory, no default)
 #'
-#' a list containing the followin datasets:
+#' a list containing the following datasets:
 #' - `cancer_record_dataset`: as output by
 #'   [nordcanpreprocessing::nordcan_processed_cancer_record_dataset]
-#' - `general_population_size_dataset`: see
-#'   [nordcanpreprocessing::nordcan_general_population_size_dataset]
-#' - `national_population_life_table`: see
-#'   [nordcanpreprocessing::nordcan_national_population_life_table]
+#' - `general_population_size_dataset`: the dataset of population sizes
+#'   as per the call for data
+#' - `national_population_life_table`: the life table as per the call for data
 #' - `cancer_death_count_dataset`: dataset containing numbers of cancer deaths;
-#'   the creation of this dataset is explained in
-#'   [nordcanpreprocessing::nordcan_processed_cancer_death_count_dataset]
+#'   see Details
+#'
+#' @details
+#' You need to form `cancer_death_count_dataset` yourself using the raw data
+#' you have using one of two methods.
+#' If you have a dataset of cancer death counts as described in the call for
+#' data, do
+#'
+#' ```
+#' cdcd <- nordcanpreprocessing::nordcan_processed_cancer_death_count_dataset(
+#'   my_raw_cdcd
+#' )
+#' ```
+#'
+#' where `my_raw_cdcd` is your dataset of cancer death counts as per the call
+#' for data.
+#'
+#' If you want to compute the counts using your cancer record dataset, do
+#'
+#' ```
+#' cdcd <- nordcanepistats::nordcanstat_count(
+#'   processed_cancer_record_dataset,
+#'   by = c("sex", "entity", "yoi", "region", "agegroup"),
+#'   subset = died_from_cancer == TRUE
+#' )
+#' data.table::setnames(cdcd,
+#'                      c("N", "yoi"), c("death_count", "year"))
+#' ```
+#'
+#' where `processed_cancer_record_dataset` is your cancer record dataset after
+#' processing
+#' (see [nordcanpreprocessing::nordcan_processed_cancer_record_dataset]),
+#' and in this example the information on who died of which cancer is identified
+#' in the logical vector `died_from_cancer`, which you need to define. It should
+#' be of length `nrow(processed_cancer_record_dataset)`. One person can
+#' naturally only die once, so there can be at most one `TRUE` value per person.
+#'
 #'
 #' @examples
 #'
+#' \dontrun{
 #' nc_stats <- nordcan_statistics_tables(
 #'   datasets = list(
-#'     cancer_record_dataset = my_crd,
-#'     cancer_death_count_dataset = my_cdcd,
-#'     general_population_size_dataset = my_gpsd,
-#'     national_population_life_table = my_lt
+#'     cancer_record_dataset = crd,
+#'     cancer_death_count_dataset = cdcd,
+#'     general_population_size_dataset = gpsd,
+#'     national_population_life_table = lt
 #'   )
 #' )
+#' }
 #' @export
 
 # ####data
