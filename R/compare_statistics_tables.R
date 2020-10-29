3e9b8e7d07a74aa678bf75ec67f6b7b2b2fac97f
Joonas Miettinen <joonas.miettinen@cancer.fi>
2020-10-29 20:22:42 +0200

    compare_statistics_tables.R x->new, y->old

--- R/compare_statistics_tables.R
+++ R/compare_statistics_tables.R
@@ -64,31 +64,31 @@ compare_nordcan_statistics_table_lists <- function(
             "comparing current_stat_table_list$", table_name,
             " to old_stat_table_list$" , table_name, "...")
     t <- proc.time()
-    x <- data.table::copy(current_stat_table_list[[table_name]])
-    y <- data.table::copy(old_stat_table_list[[table_name]])
+    new <- data.table::copy(current_stat_table_list[[table_name]])
+    old <- data.table::copy(old_stat_table_list[[table_name]])
     xy_stratum_col_nm_set <- intersect(
       stratum_col_nm_set,
-      intersect(names(x), names(y))
+      intersect(names(new), names(old))
     )
-    x <- x[
-      i = y,
+    new <- new[
+      i = old,
       on = eval(xy_stratum_col_nm_set),
       nomatch = 0L,
       j = .SD,
-      .SDcols = names(x)
+      .SDcols = names(new)
     ]
-    data.table::setkeyv(x, xy_stratum_col_nm_set)
-    y <- y[
-      i = x,
+    data.table::setkeyv(new, xy_stratum_col_nm_set)
+    old <- old[
+      i = new,
       on = eval(xy_stratum_col_nm_set),
       nomatch = 0L,
       j = .SD,
-      .SDcols = names(y)
+      .SDcols = names(old)
     ]
-    data.table::setkeyv(y, xy_stratum_col_nm_set)
-    comparison_dt <- compare_tables(x = x, y = y, table_name = table_name)
+    data.table::setkeyv(old, xy_stratum_col_nm_set)
+    comparison_dt <- compare_tables(new = new, old = old, table_name = table_name)
     out <- cbind(
-      x[j = .SD, .SDcols = xy_stratum_col_nm_set],
+      new[j = .SD, .SDcols = xy_stratum_col_nm_set],
       comparison_dt
     )
     message("* nordcanepistats::compare_nordcan_statistics_table_lists: ",
@@ -140,11 +140,11 @@ compare_nordcan_statistics_table_lists <- function(
         n_p_value_lte_005 = sum(dt$p_value < 0.05, na.rm = TRUE),
         min_stat_value = min(dt$stat_value, na.rm = TRUE),
         median_stat_value = stats::median(dt$stat_value, na.rm = TRUE),
-        min_stat_value = max(dt$stat_value, na.rm = TRUE),
+        max_stat_value = max(dt$stat_value, na.rm = TRUE),
         n_na_p_value = sum(is.na(dt$p_value)),
-        n_na_x = sum(is.na(dt$x)),
-        n_na_y = sum(is.na(dt$y)),
-        n_na_x_and_na_y = sum(is.na(dt$x) & is.na(dt$y))
+        n_na_x = sum(is.na(dt$new)),
+        n_na_y = sum(is.na(dt$old)),
+        n_na_x_and_na_y = sum(is.na(dt$new) & is.na(dt$old))
       ),
       keyby = c("stat_type", "column_name")
     ]
@@ -159,37 +159,37 @@ compare_nordcan_statistics_table_lists <- function(
 
 
 compare_tables <- function(
-  x,
-  y,
+  new,
+  old,
   table_name
 ) {
-  dbc::assert_is_data.table(x)
-  dbc::assert_is_data.table(y)
+  dbc::assert_is_data.table(new)
+  dbc::assert_is_data.table(old)
   dbc::assert_is_character_nonNA_atom(table_name)
 
   switch(
     table_name,
     cancer_record_count_dataset = compare_count_tables(
-      x = x, y = y, count_col_nm = "cancer_record_count"
+      new = new, old = old, count_col_nm = "cancer_record_count"
     ),
     cancer_death_count_dataset = compare_count_tables(
-      x = x, y = y, count_col_nm = "cancer_death_count"
+      new = new, old = old, count_col_nm = "cancer_death_count"
     ),
     prevalent_patient_count_dataset = compare_count_tables(
-      x = x, y = y, count_col_nm = "prevalent_patient_count"
+      new = new, old = old, count_col_nm = "prevalent_patient_count"
     ),
     imp_quality_statistics_dataset = compare_imp_quality_statistics_tables(
-      x = x, y = y,
-      prop_col_nms = names(x)[grepl("percent", names(x))],
+      new = new, old = old,
+      prop_col_nms = names(new)[grepl("percent", names(new))],
       count_col_nm = "cancer_record_count"
     ),
     survival_quality_statistics_dataset = compare_survival_quality_statistics_tables(
-      x = x, y = y,
-      prop_col_nms = names(x)[grepl("percent", names(x))],
+      new = new, old = old,
+      prop_col_nms = names(new)[grepl("percent", names(new))],
       count_col_nm = "cancer_record_count"
     ),
     survival_statistics_dataset = compare_survival_statistics_tables(
-      x = x, y = y,
+      new = new, old = old,
       surv_col_nm = "cns"
     ),
     stop("Internal error: no comparison function defined for table named ",
@@ -202,36 +202,36 @@ compare_tables <- function(
 
 
 compare_imp_quality_statistics_tables <- function(
-  x, y, prop_col_nms, count_col_nm
+  new, old, prop_col_nms, count_col_nm
   ) {
   dt <- data.table::rbindlist(lapply(prop_col_nms, function(prop_col_nm) {
     compare_proportion_tables(
-      x = x, y = y, prop_col_nm = prop_col_nm
+      new = new, old = old, prop_col_nm = prop_col_nm
     )
   }))
   dt <- rbind(
     dt,
-    compare_rate_tables(x = x, y = y, rate_col_nm = "mi_ratio",
+    compare_rate_tables(new = new, old = old, rate_col_nm = "mi_ratio",
                         count_col_nm = count_col_nm)
   )
   return(dt[])
 }
 
 compare_survival_quality_statistics_tables <- function(
-  x, y, prop_col_nms, count_col_nm
+  new, old, prop_col_nms, count_col_nm
 ) {
   dbc::assert_is_data.table_with_required_names(
-    x,
+    new,
     required_names = c(prop_col_nms, count_col_nm)
   )
   dbc::assert_is_data.table_with_required_names(
-    y,
+    old,
     required_names = c(prop_col_nms, count_col_nm)
   )
   dt <- data.table::rbindlist(lapply(prop_col_nms, function(prop_col_nm) {
     compare_proportion_tables(
-      x = x,
-      y = y, prop_col_nm = prop_col_nm, count_col_nm = count_col_nm
+      new = new,
+      old = old, prop_col_nm = prop_col_nm, count_col_nm = count_col_nm
     )
   }))
   return(dt[])
@@ -239,43 +239,43 @@ compare_survival_quality_statistics_tables <- function(
 
 #' @importFrom data.table :=
 compare_survival_statistics_tables <- function(
-  x, y, surv_col_nm
+  new, old, surv_col_nm
 ) {
-  dt <- compare_survivals(x = x[[surv_col_nm]], y = y[[surv_col_nm]])
+  dt <- compare_survivals(new = new[[surv_col_nm]], old = old[[surv_col_nm]])
 
   dt[, "column_name" := surv_col_nm]
   return(dt[])
 }
 
 #' @importFrom data.table :=
-compare_count_tables <- function(x, y, count_col_nm) {
+compare_count_tables <- function(new, old, count_col_nm) {
   dt <- compare_counts(
-    x = x[[count_col_nm]],
-    y = y[[count_col_nm]]
+    new = new[[count_col_nm]],
+    old = old[[count_col_nm]]
   )
   dt[, "column_name" := count_col_nm]
   return(dt[])
 }
 
 #' @importFrom data.table :=
-compare_rate_tables <- function(x, y, count_col_nm, rate_col_nm) {
+compare_rate_tables <- function(new, old, count_col_nm, rate_col_nm) {
   dt <- compare_rates(
-    x = x[[rate_col_nm]],
-    y = y[[rate_col_nm]],
-    x_count = x[[count_col_nm]],
-    y_count = y[[count_col_nm]]
+    new = new[[rate_col_nm]],
+    old = old[[rate_col_nm]],
+    x_count = new[[count_col_nm]],
+    y_count = old[[count_col_nm]]
   )
   dt[, "column_name" := rate_col_nm]
   return(dt[])
 }
 
 #' @importFrom data.table :=
-compare_proportion_tables <- function(x, y, prop_col_nm, count_col_nm) {
+compare_proportion_tables <- function(new, old, prop_col_nm, count_col_nm) {
   dt <- compare_proportions(
-    x = x[[prop_col_nm]],
-    y = y[[prop_col_nm]],
-    x_count = x[[count_col_nm]],
-    y_count = y[[count_col_nm]]
+    new = new[[prop_col_nm]],
+    old = old[[prop_col_nm]],
+    x_count = new[[count_col_nm]],
+    y_count = old[[count_col_nm]]
   )
   dt[, "column_name" := prop_col_nm]
   return(dt[])
@@ -284,37 +284,37 @@ compare_proportion_tables <- function(x, y, prop_col_nm, count_col_nm) {
 
 #' @importFrom data.table :=
 compare_counts <- function(
-  x,
-  y
+  new,
+  old
 ) {
-  dbc::assert_prod_input_is_integer_gtezero_vector(x)
-  dbc::assert_prod_input_is_integer_gtezero_vector(y)
+  dbc::assert_prod_input_is_integer_gtezero_vector(new)
+  dbc::assert_prod_input_is_integer_gtezero_vector(old)
 
-  dt <- data.table::setDT(list(x = x, y = y))
+  dt <- data.table::setDT(list(new = new, old = old))
   dt[, "stat_type" := "count_x_minus_y"]
-  dt[, "stat_value" := dt$x - dt$y]
-  h0_means <- (dt$x + dt$y) / 2.0
+  dt[, "stat_value" := dt$new - dt$old]
+  h0_means <- (dt$new + dt$old) / 2.0
   dt[
     j = "p_value" := 2 * (1 - skellam_cdf(q = abs(dt$stat_value),
                                           mu1 = h0_means, mu2 = h0_means))
 
   ]
-  dt[dt$x == 0L & dt$y == 0L, "p_value" := NA_real_]
+  dt[dt$new == 0L & dt$old == 0L, "p_value" := NA_real_]
   return(dt[])
 }
 
 
 #' @importFrom data.table := .N
 compare_survivals <- function(
-  x,
-  y
+  new,
+  old
 ) {
-  dbc::assert_prod_input_is_number_gtezero_vector(x)
-  dbc::assert_prod_input_is_number_gtezero_vector(y)
+  dbc::assert_prod_input_is_number_gtezero_vector(new)
+  dbc::assert_prod_input_is_number_gtezero_vector(old)
 
-  dt <- data.table::setDT(list(x = x, y = y))
+  dt <- data.table::setDT(list(new = new, old = old))
   dt[, "stat_type" := "survival_x_minus_y"]
-  dt[, "stat_value" := dt$x - dt$y]
+  dt[, "stat_value" := dt$new - dt$old]
   dt[, "p_value" := NA_real_]
   return(dt[])
 }
@@ -322,29 +322,29 @@ compare_survivals <- function(
 
 #' @importFrom data.table :=
 compare_proportions <- function(
-  x,
-  y,
+  new,
+  old,
   x_count,
   y_count
 ) {
-  dbc::assert_prod_input_is_number_vector(x)
-  dbc::assert_prod_input_is_number_vector(y)
+  dbc::assert_prod_input_is_number_vector(new)
+  dbc::assert_prod_input_is_number_vector(old)
   eval_env <- environment()
   dbc::report_to_assertion(
     dbc::tests_to_report(
-      tests = c("is.na(x) | (x >= 0.0 & x <= 100.0)",
-                "is.na(y) | (y >= 0.0 & y <= 100.0)"),
+      tests = c("is.na(new) | (new >= 0.0 & new <= 100.0)",
+                "is.na(old) | (old >= 0.0 & old <= 100.0)"),
       fail_messages = c(
-        "first five invalid values: ${deparse(utils::head(x[wh_fail], 5))}",
-        "first five invalid values: ${deparse(utils::head(y[wh_fail], 5))}"
+        "first five invalid values: ${deparse(utils::head(new[wh_fail], 5))}",
+        "first five invalid values: ${deparse(utils::head(old[wh_fail], 5))}"
       ),
       env = eval_env
     )
   )
 
-  dt <- data.table::setDT(list(x = x, y = y))
+  dt <- data.table::setDT(list(new = new, old = old))
   dt[, "stat_type" := "proportion_x_minus_y"]
-  dt[, "stat_value" := dt$x - dt$y]
+  dt[, "stat_value" := dt$new - dt$old]
   dt[, "p_value" := NA_real_]
   return(dt[])
 }
@@ -353,19 +353,19 @@ compare_proportions <- function(
 
 #' @importFrom data.table :=
 compare_rates <- function(
-  x,
-  y,
+  new,
+  old,
   x_count,
   y_count
 ) {
   # in the case of mi_ratio, this is a poisson model where the cancer record
   # count is the poisson offset.
 
-  dt <- data.table::setDT(list(x = x, y = y))
+  dt <- data.table::setDT(list(new = new, old = old))
   dt[, "stat_type" := "ratio_x_minus_y"]
-  dt[, "stat_value" := dt$x - dt$y]
-  x_as_count <- as.integer(round(x * x_count))
-  y_as_count <- as.integer(round(y * y_count))
+  dt[, "stat_value" := dt$new - dt$old]
+  x_as_count <- as.integer(round(new * x_count))
+  y_as_count <- as.integer(round(old * y_count))
   abs_count_diff <- abs(x_as_count - y_as_count)
   h0_means  <- (x_as_count + y_as_count) / 2L
 
