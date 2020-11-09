e8a45dca4609aab26f5b841da22c4f6018fb08d3
Joonas Miettinen <joonas.miettinen@cancer.fi>
2020-11-09 14:48:30 +0200

    sysdata.R: ensure no duplicate file names / object names in statistics_tables_names

--- data-raw/sysdata.R
+++ data-raw/sysdata.R
@@ -4,6 +4,12 @@
 statistics_tables_names <- data.table::fread(
   "data-raw/statistics_tables_names.csv"
 )
+
+stopifnot(
+  !duplicated(statistics_tables_names$object_name),
+  !duplicated(statistics_tables_names$csv_file_name)
+)
+
 usethis::use_data(statistics_tables_names, internal = TRUE, overwrite = TRUE)
 
 
