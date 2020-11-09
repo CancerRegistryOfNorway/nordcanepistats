ede8665f438b2fd327a9ee809f95331d839250b6
Joonas Miettinen <joonas.miettinen@cancer.fi>
2020-11-09 14:47:31 +0200

    statistics_tables_names.csv: bugfix
    duplicated survival_statistics_dataset fixed

--- data-raw/sysdata.R
+++ data-raw/sysdata.R
@@ -4,6 +4,6 @@
 statistics_tables_names <- data.table::fread(
   "data-raw/statistics_tables_names.csv"
 )
-usethis::use_data(statistics_tables_names, internal = TRUE)
+usethis::use_data(statistics_tables_names, internal = TRUE, overwrite = TRUE)
 
 
